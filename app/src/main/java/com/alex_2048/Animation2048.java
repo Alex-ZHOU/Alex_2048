c471c8a5fd24d3a5bf7c87a1871f20e3bbdaafb0
Alex940414 <850946554@qq.com>
2016-04-08 15:54:50 +0800

    add copyright

--- app/src/main/java/com/alex_2048/Animation2048.java
+++ app/src/main/java/com/alex_2048/Animation2048.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 AlexZHOU
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.alex_2048;
 
 import android.alex.utils.TouchEventDetection;
@@ -120,13 +136,16 @@ public class Animation2048 extends Animation {
           flag = true;
           translateAnimation[j][i].setAnimationListener(new AnimationListener() {
 
-            @Override public void onAnimationStart(Animation animation) {
+            @Override
+            public void onAnimationStart(Animation animation) {
             }
 
-            @Override public void onAnimationRepeat(Animation animation) {
+            @Override
+            public void onAnimationRepeat(Animation animation) {
             }
 
-            @Override public void onAnimationEnd(Animation animation) {
+            @Override
+            public void onAnimationEnd(Animation animation) {
               handler.sendEmptyMessage(TouchEventDetection.ACTION_UP);
             }
           });
@@ -205,13 +224,16 @@ public class Animation2048 extends Animation {
           flag = true;
           translateAnimation[j][i].setAnimationListener(new AnimationListener() {
 
-            @Override public void onAnimationStart(Animation animation) {
+            @Override
+            public void onAnimationStart(Animation animation) {
             }
 
-            @Override public void onAnimationRepeat(Animation animation) {
+            @Override
+            public void onAnimationRepeat(Animation animation) {
             }
 
-            @Override public void onAnimationEnd(Animation animation) {
+            @Override
+            public void onAnimationEnd(Animation animation) {
               handler.sendEmptyMessage(TouchEventDetection.ACTION_DOWN);
             }
           });
@@ -290,13 +312,16 @@ public class Animation2048 extends Animation {
           flag = true;
           translateAnimation[i][j].setAnimationListener(new AnimationListener() {
 
-            @Override public void onAnimationStart(Animation animation) {
+            @Override
+            public void onAnimationStart(Animation animation) {
             }
 
-            @Override public void onAnimationRepeat(Animation animation) {
+            @Override
+            public void onAnimationRepeat(Animation animation) {
             }
 
-            @Override public void onAnimationEnd(Animation animation) {
+            @Override
+            public void onAnimationEnd(Animation animation) {
               handler.sendEmptyMessage(TouchEventDetection.ACTION_RIGHT);
             }
           });
@@ -376,13 +401,16 @@ public class Animation2048 extends Animation {
           flag = true;
           translateAnimation[i][j].setAnimationListener(new AnimationListener() {
 
-            @Override public void onAnimationStart(Animation animation) {
+            @Override
+            public void onAnimationStart(Animation animation) {
             }
 
-            @Override public void onAnimationRepeat(Animation animation) {
+            @Override
+            public void onAnimationRepeat(Animation animation) {
             }
 
-            @Override public void onAnimationEnd(Animation animation) {
+            @Override
+            public void onAnimationEnd(Animation animation) {
               handler.sendEmptyMessage(TouchEventDetection.ACTION_LEFT);
             }
           });
