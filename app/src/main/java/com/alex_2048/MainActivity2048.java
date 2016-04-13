5c02d8e8bc4ba761a33800deb3002de39abd39b3
Alex-ZHOU <zxb850946554>
2016-04-13 20:12:00 +0800

    201604132011

--- app/src/main/java/com/alex_2048/MainActivity2048.java
+++ app/src/main/java/com/alex_2048/MainActivity2048.java
@@ -40,6 +40,7 @@ public class MainActivity2048 extends Activity implements OnClickListener {
 
   private int[][] data = new int[4][4];
 
+
   private int score = 0;
 
   private TouchEventDetection touchEventDetection;
@@ -154,34 +155,6 @@ public class MainActivity2048 extends Activity implements OnClickListener {
     //}
   }
 
-  /**
-   * Perform any final cleanup before an activity is destroyed.  This can
-   * happen either because the activity is finishing (someone called
-   * {@link #finish} on it, or because the system is temporarily destroying
-   * this instance of the activity to save space.  You can distinguish
-   * between these two scenarios with the {@link #isFinishing} method.
-   *
-   * <p><em>Note: do not count on this method being called as a place for
-   * saving data! For example, if an activity is editing data in a content
-   * provider, those edits should be committed in either {@link #onPause} or
-   * {@link #onSaveInstanceState}, not here.</em> This method is usually implemented to
-   * free resources like threads that are associated with an activity, so
-   * that a destroyed activity does not leave such things around while the
-   * rest of its application is still running.  There are situations where
-   * the system will simply kill the activity's hosting process without
-   * calling this method (or any others) in it, so it should not be used to
-   * do things that are intended to remain around after the process goes
-   * away.
-   *
-   * <p><em>Derived classes must call through to the super class's
-   * implementation of this method.  If they do not, an exception will be
-   * thrown.</em></p>
-   *
-   * @see #onPause
-   * @see #onStop
-   * @see #finish
-   * @see #isFinishing
-   */
   @Override
   protected void onDestroy() {
     super.onDestroy();
